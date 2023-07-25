9e6a4219d33d78b32b0614e39b76bc8cf5d08021
acalvom <andreac.3108@gmail.com>
2023-07-25 10:15:57 +0200

    feat: books module & get all endpoint

---
+++ src/books/books.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class BooksService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createBookDto: CreateBookDto) {
+    return 'This action adds a new book';
+  }
+
+  findAll() {
+    return this.prisma.book.findMany();
+  }
+
+  findRead() {
+    return this.prisma.book.findMany({ where: { isRead: true } });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
